3338d5d0a157c8ccadd7709b3b9aaf6c114b130e
Mansa Samlafo <msamlafo@gmail.com>
2021-02-03 13:57:56 -0500

    fix for heroku 1

--- src/Common/Environment.tsx
+++ src/Common/Environment.tsx
@@ -9,7 +9,7 @@ export const CLOUDINARY_API_KEY =
 export const defaultHomePage = '/poetry';
 export const adminHomePage = '/issue';
 
-export let BASE_API_URL = "";
+export let BASE_API_URL = '';
 
 switch (window.location.hostname) {
   // this is the local host name of your react app
@@ -20,7 +20,6 @@ switch (window.location.hostname) {
   // this is the deployed react application
   case 'mds-openmicclient.herokuapp.com':
     // this is the full url of your deployed API
-    BASE_API_URL = 'https://mds-openmicserver.herokuapp.com/'
+    BASE_API_URL = 'https://mds-openmicserver.herokuapp.com';
     break;
 }
-
